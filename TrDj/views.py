48eb3ee72f43f8d40a40be65059c569464f53278
Gautam Sood <gauty22@gmail.com>
2022-02-23 14:51:51 +0530

    Django string and response()

--- TrDj/views.py
+++ TrDj/views.py
@@ -1,16 +1,33 @@
 """ 
     To render HTML pages 
 """
-
+import random
 from django.http import HttpResponse
 
-HTML_STRING = """
-<h1>Hello World</h1>
-"""
 
-def home(request):
+
+def home_view(request):
     """
     take in a request (Django sends request)
     Return HTML as a response (We pick to return the response)
     """
+    
+    name = "Justin"
+    number = random.randint(10,1233123)
+
+    # Django Database
+
+
+
+    # Django templates
+
+    H1_STRING = f"""
+    <h1>Hello {name} - {number}!</h1>
+    """
+    P_STRING = f"""
+    <p>Hello {name} - {number}!</p>
+    """
+
+
+    HTML_STRING = H1_STRING + P_STRING
     return HttpResponse(HTML_STRING)
